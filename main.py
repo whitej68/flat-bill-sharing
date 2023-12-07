0bbfb9fd3415b5a451019793ad10ddbd58fda8e6
whitej68 <114672020+whitej68@users.noreply.github.com>
2023-12-07 15:28:15 -0500

    Implemented pdf report class generate method

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from fpdf import FPDF
+
+
 class Bill:
     """
     Object that contains data about a bill, such as total amount
@@ -36,14 +39,39 @@ class PdfReport:
         self.filename = filename
 
     def generate(self, flatmate1, flatmate2, bill):
-        pass
+        pdf = FPDF(orientation='P', unit='pt', format='A4')
+        pdf.add_page()
+
+        # Add icon
+        pdf.image(name="house.png", w=30, h=30)
+
+        # Insert titles
+        pdf.set_font(family='Times', size=24, style='B')
+        pdf.cell(w=0, h=80, txt="Flatmates Bill", border=0, align="C", ln=1)
+
+        # Insert Period label and value
+        pdf.set_font(family='Times', size=14, style='B')
+        pdf.cell(w=100, h=40, txt="Period:", border=0)
+        pdf.cell(w=150, h=40, txt=bill.period, border=0, ln=1)
+
+        # Insert name and due amount of the first flatmate
+        pdf.set_font(family='Times', size=12)
+        pdf.cell(w=100, h=25, txt=flatmate1.name, border=0)
+        pdf.cell(w=150, h=25, txt=str(round(flatmate1.pays(bill, flatmate2), 2)), border=0, ln=1)
+
+        # Insert name and due amount of the second flatmate
+        pdf.cell(w=100, h=25, txt=flatmate2.name, border=0)
+        pdf.cell(w=150, h=25, txt=str(round(flatmate2.pays(bill, flatmate1), 2)), border=0, ln=1)
+
+        pdf.output(self.filename)
 
 
-the_bill = Bill(amount=120, period="March 2021")
+the_bill = Bill(amount=120, period="March 2023")
 john = Flatmate(name="John", days_in_house=20)
 mary = Flatmate(name="Mary", days_in_house=25)
 
 print("John pays: ", john.pays(bill=the_bill, flatmate2=mary))
 print("Mary pays: ", mary.pays(bill=the_bill, flatmate2=john))
 
-
+pdf_report = PdfReport(filename="Report1.pdf")
+pdf_report.generate(flatmate1=john, flatmate2=mary, bill=the_bill)
