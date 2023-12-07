467a03b763f5a1aae72df922f4ba7d39b57fb805
whitej68 <114672020+whitej68@users.noreply.github.com>
2023-12-07 16:53:32 -0500

    Refactored classes into separate py files

---
+++ reports.py
@@ -0,0 +1,43 @@
+import webbrowser
+from fpdf import FPDF
+import os
+
+class PdfReport:
+    """
+    Creates a pdf file that contains data about
+    the flatmates such as their names, their due amount
+    and the period of the bill
+    """
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def generate(self, flatmate1, flatmate2, bill):
+        pdf = FPDF(orientation='P', unit='pt', format='A4')
+        pdf.add_page()
+
+        # Add icon
+        pdf.image(name="files/house.png", w=30, h=30)
+
+        # Insert titles
+        pdf.set_font(family='Times', size=24, style='B')
+        pdf.cell(w=0, h=80, txt="Flatmates Bill", border=0, align="C", ln=1)
+
+        # Insert Period label and value
+        pdf.set_font(family='Times', size=14, style='B')
+        pdf.cell(w=100, h=40, txt="Period:", border=0)
+        pdf.cell(w=150, h=40, txt=bill.period, border=0, ln=1)
+
+        # Insert name and due amount of the first flatmate
+        pdf.set_font(family='Times', size=12)
+        pdf.cell(w=100, h=25, txt=flatmate1.name, border=0)
+        pdf.cell(w=150, h=25, txt=str(round(flatmate1.pays(bill, flatmate2), 2)), border=0, ln=1)
+
+        # Insert name and due amount of the second flatmate
+        pdf.cell(w=100, h=25, txt=flatmate2.name, border=0)
+        pdf.cell(w=150, h=25, txt=str(round(flatmate2.pays(bill, flatmate1), 2)), border=0, ln=1)
+
+        # Change directory to files and generate and open PDF
+        os.chdir("files")
+        pdf.output(self.filename)
+        webbrowser.open(self.filename)
